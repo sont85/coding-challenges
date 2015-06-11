205491e51d872248ef6a234767ab600d351eabb4
Son Truong <sont85@gmail.com>
2015-06-11 11:18:09 -0700

    minimize DOM access

--- karma/script.js
+++ karma/script.js
@@ -1,3 +1,5 @@
+// Code goes here
+
 var $$ = {
   storeData: function() {
     localStorage["karma.data"] = JSON.stringify(this.data);
@@ -8,51 +10,53 @@ var $$ = {
   leaderboard: function() {
     return this.data.sort(this.compare);
   },
-  compare: function (a, b){
+  compare: function(a, b) {
     return b.points - a.points;
   },
-  // modifyPointsFor(0, 2);
   modifyPointsFor: function(indexInArray, newPoints) {
     this.data[indexInArray].points = newPoints;
     this.storeData();
     this.cloner();
   },
-  cloner: function(){
-  $("#ppl").empty();
-  var sorted = $$.leaderboard();
-  var $template = $(".person:first"), $clonedLi;
-  var ppl = sorted.map(function(p, i) {
-    $clonedLi = $template.clone().show();
-    $clonedLi.data("order", i);
-    $clonedLi.find(".name").text(p.name);
-    $clonedLi.find(".points").text(p.points);
-    $clonedLi.find("input").val(p.points);
-    return $clonedLi;
-  });
+  cloner: function() {
+    $("#ppl").empty();
+    var sorted = $$.leaderboard();
+    var $template = $(".person:first"),
+      $clonedLi;
+    var ppl = sorted.map(function(p, i) {
+      $clonedLi = $template.clone().show();
+      $clonedLi.data("order", i);
+      $clonedLi.find(".name").text(p.name);
+      $clonedLi.find(".points").text(p.points);
+      $clonedLi.find("input").val(p.points);
+      return $clonedLi;
+    });
 
-  $("#ppl").append(ppl);
-
+    $("#ppl").append(ppl);
   },
-  eventHandler: function(){
-      $("#ppl").on("dblclick", ".points", function(event) {
-          $(this).hide();
-          $(this).parent(".person").find(".input").show(); //problem
-      }).on("keyup", "input", function(event) {
-        if (event.which === 13) {
-            var person = $(this).parents(".person");
-            var personIndex = person.data("order"); //problem
-            var newVal = $(this).val();
-            $$.modifyPointsFor(personIndex, newVal); 
-            $(this).parent().prev(".points").text(newVal); //problem
-            $(this).parent().hide(); //problem
-            $(".points").show();
+  eventHandler: function() {
+    $("#ppl").on("dblclick", ".points", function(event) {
+        var $points = $(this)
+        $points.hide();
+        $points.next(".input").show();
+    }).on("keyup", "input", function(event) {
+      if (event.which === 13) {
+        var $input = $(this);
+        var $person = $input.parents(".person")
+        var personIndex = $person.data("order"); 
+        var newVal = $input.val();
+        $$.modifyPointsFor(personIndex, newVal);
+        $person.find(".input").hide()
+        $person.find(".points").text(newVal).show()
+        $(".points").show();
 
-        }
-      });
+      }
+    });
 
   },
 
   data: []
+  data:""
 }
 
 
@@ -63,14 +67,3 @@ $(document).ready(function() {
 
 
 })
-
-
-
-
-
-
-
-
-
-
-
